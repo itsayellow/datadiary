f8688a7b62b3764e285d7732aa5e467c6f44cfa1
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-07 19:35:35 -0800

    Add url, reformat via black.

--- setup.py
+++ setup.py
@@ -1,44 +1,39 @@
 # setup for datadiary
 
-#import os.path
+# import os.path
 from setuptools import setup, find_packages
 
-#here = os.path.abspath(os.path.dirname(__file__))
+# here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-        name='datadiary',
-        version='0.2',
-        description='Generate html reports.',
-        author='Matthew A. Clapp',
-        author_email='itsayellow+dev@gmail.com',
-        license='MIT',
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3'
-            ],
-        keywords='report',
-        include_package_data=True,# so we get html files in datadiary/templates
-        packages=[
-            'datadiary'
-            ],
-        install_requires=[
-            'Jinja2',
-            'pandas',
-            'numpy',
-            'matplotlib',
-            'tensorflow',
-            'keras',
-            'pydot',
-            #'Pillow',
-            'imagesize',
-            'tqdm',
-            ],
-        entry_points={
-            'console_scripts':[
-                'diarygen=datadiary.command_line:main'
-                ]
-            },
-        python_requires='>=3',
-        )
+    name="datadiary",
+    version="0.2",
+    description="Generate html reports.",
+    author="Matthew A. Clapp",
+    author_email="itsayellow+dev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="report",
+    url="https://github.com/itsayellow/datadiary",
+    include_package_data=True,  # so we get html files in datadiary/templates
+    packages=["datadiary"],
+    install_requires=[
+        "Jinja2",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "tensorflow",
+        "keras",
+        "pydot",
+        #'Pillow',
+        "imagesize",
+        "tqdm",
+    ],
+    entry_points={"console_scripts": ["diarygen=datadiary.command_line:main"]},
+    python_requires=">=3",
+)
